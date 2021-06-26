10b6610e7358d606737e34e2c2d7d5e20703682b
jcpeters <petersjoe@gmail.com>
2021-06-26 13:00:45 -0700

    4 files modified, 1 added, 1 deleted

---
+++ App.tsx
@@ -0,0 +1,73 @@
+import { FunctionComponent, useState } from "react";
+import "./styles.css";
+
+interface fizzBuzz {
+  fb_number: number;
+  status: string;
+}
+
+export const App: FunctionComponent = () => {
+  const [fizzBuzzIndex, setFizzBuzzIndex] = useState(3);
+  const [currNum, setCurrNum] = useState(3);
+
+  const handleClick = () => {
+    setFizzBuzzIndex(fizzBuzzIndex + 1);
+  };
+
+  const getLabel = () => {
+    if (fizzBuzzIndex % 3 === 0 && fizzBuzzIndex % 5 === 0) {
+      return "Fizz Buzz";
+    }
+    if (fizzBuzzIndex % 3 === 0) {
+      return "Fizz";
+    }
+    if (fizzBuzzIndex % 5 === 0) {
+      return "Buzz";
+    }
+    return `${fizzBuzzIndex}`;
+  };
+
+  const fizzBuzzMe = () => {
+    let numArr: Array<fizzBuzz> = [];
+
+    for (let i = 0; i < 10; i++) {
+      let bar = getLabel();
+      let poop: fizzBuzz = {
+        fb_number: fizzBuzzIndex,
+        status: bar
+      };
+      numArr.push(poop);
+      console.log(poop);
+    }
+    const listItems = numArr.map((index) => (
+      <li>
+        {index.fb_number} {index.status}
+      </li>
+    ));
+    return <ul>{listItems}</ul>;
+  };
+
+  return (
+    <>
+      <div>
+        <button
+          data-testid="Button1"
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          Index: {fizzBuzzIndex}
+        </button>
+        <button
+          data-testid="Button2"
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          Result: {getLabel()}
+        </button>
+        {fizzBuzzMe()} {fizzBuzzIndex}
+      </div>
+    </>
+  );
+};
